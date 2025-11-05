6fe2ac7a63648737acdb3546d1cf7514c6361db1
Patrik Scerba <patrikscerba@gmail.com>
2025-11-05 15:41:31 +0100

    feat: vytvorenie databázového pripojenia (DatabazaPripojenie.java)
    - pridaná trieda DatabazaPripojenie s JDBC pripojením k MySQL
    - úspešne otestované pripojenie k databáze evidencia_klientov
    - konfigurácia MySQL: názov databázy evidencia_klientov, používateľ root, heslo prázdne
    - trieda slúži ako centrálny bod pre vytváranie databázového spojenia

--- src/main/java/sk/patrikscerba/db/DatabazaPripojenie.java
+++ src/main/java/sk/patrikscerba/db/DatabazaPripojenie.java
@@ -1,4 +1,17 @@
 package sk.patrikscerba.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabazaPripojenie {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/evidencia_klientov";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    // Vráti pripojenie k databáze
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
